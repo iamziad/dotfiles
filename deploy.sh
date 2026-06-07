4f618ea7cd33a991c4859d5a4905bd21b7af61cb
Ziad Ahmed <dev.ziadahmed@gmail.com>
2026-06-07 10:30:55 +0300

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PROFILE=$1
+
+DESKTOP_GUI=(git alacritty vim tmux)
+DESKTOP_I3=("${DESKTOP_GUI[@]}" i3 rofi redshift dunst xorg Scripts)
+
+if [ "$PROFILE" = "desktop-gui" ]; then
+    echo "$0: deploying $1.."
+    stow -v "${DESKTOP_GUI[@]}"
+    
+    echo -e "$0: deploying system.."
+    sudo stow -v -t / system
+
+else
+    echo "$0: deploying i3 profile.."
+    stow -v "${DESKTOP_I3[@]}"
+
+    echo -e "$0: deploying system.."
+    sudo stow -v -t / system
+fi
+
+echo -e "$0: done!"
