a5ad1e3cc30642ae300eae093b10e92938dc4b7d
Ziad Ahmed <dev.ziadahmed@gmail.com>
2026-06-14 21:18:04 +0300

    u

--- Scripts/Scripts/feh.sh
+++ Scripts/Scripts/feh.sh
@@ -1,1 +1,2 @@
-feh --bg-scale --no-fehbg "$(find ~/Wallpapers/ -maxdepth 1 -type f | shuf -n 1)"
+# feh --bg-scale --no-fehbg "$(find ~/Wallpapers/ -maxdepth 1 -type f | shuf -n 1)"
+feh --bg-scale --no-fehbg "/home/ziad/Wallpapers/bliss.jpg"
