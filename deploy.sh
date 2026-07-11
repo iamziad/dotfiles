36fdee02971e37391c1eb033bd93bef0a37d0ed6
Ziad Ahmed <dev.ziadahmed@gmail.com>
2026-07-11 14:48:57 +0300

    u

--- deploy.sh
+++ deploy.sh
@@ -5,34 +5,17 @@
 #  - CPU load
 #  - CPU temp
 
-PROFILE=$1
-
 if ! command -v stow &> /dev/null; then
     echo "Error: 'stow' is not installed." >&2
     exit 1
 fi
 
+mkdir -p \
+	"$HOME/.config" \
+	"$HOME/.local/share" \
+	"$HOME/.local/bin" \
+	"$HOME/.local/state" \
+	"$HOME/.cache"
 
-DESKTOP_GUI=(runit git alacritty vim tmux bash)
-DESKTOP_I3=("${DESKTOP_GUI[@]}" bin i3 x11 redshift dunst picom mimeapps gtk)
+echo "$0: Created home skeleton"
 
-if [ "$PROFILE" = "desktop-gui" ]; then
-    echo "$0: deploying $1.."
-    stow -v "${DESKTOP_GUI[@]}"
-else
-    echo "$0: deploying i3 profile.."
-
-    mkdir -p \
-	    "$HOME/.config" \
-	    "$HOME/.local/share" \
-	    "$HOME/.cache" \
-	    "$HOME/.local/state"
-
-    echo "$0: Created home skeleton"
-    stow -v "${DESKTOP_I3[@]}"
-fi
-
-#echo -e "$0: deploying system.."
-#sudo stow -v -t / system
-
-echo -e "$0: done!"
